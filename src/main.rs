28619aa6ee088eb1001cc249cf77c9828bc62802
Carter Tomlenovich <cartertomlenovich@gmail.com>
2025-10-03 10:18:53 -0400

    add changes

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,37 @@
 use dotenv::dotenv;
-use graph_rs_sdk::{http::HttpResponseExt, identity::{ConfidentialClientApplication, EnvironmentCredential}, *};
+use graph_rs_sdk::{
+    http::HttpResponseExt,
+    identity::{ConfidentialClientApplication, EnvironmentCredential},
+    *,
+};
 use log::info;
 mod models;
 use crate::models::{App, Owners};
 use reqwest::header::HeaderName;
 use reqwest::header::HeaderValue;
 
-
-pub async fn get_applications_with_owners(client: &GraphClient, ids: Vec<String>) -> anyhow::Result<Vec<App>> {
+pub async fn get_applications_with_owners(
+    client: &GraphClient,
+    ids: Vec<String>,
+) -> anyhow::Result<Vec<App>> {
     let mut apps: Vec<App> = Vec::new();
     for id in ids {
-        let application_response = client.application(&id).get_application().select(&["id", "appId", "displayName", "passwordCredentials"]).send().await?;
+        let application_response = client
+            .application(&id)
+            .get_application()
+            .select(&["id", "appId", "displayName", "passwordCredentials"])
+            .send()
+            .await?;
 
         let mut application: App = application_response.json().await?;
 
-        let owners_response = client.application(&id).owners().list_owners().select(&["id", "displayName", "mail", "userPrincipalName"]).send().await?;
+        let owners_response = client
+            .application(&id)
+            .owners()
+            .list_owners()
+            .select(&["id", "displayName", "mail", "userPrincipalName"])
+            .send()
+            .await?;
 
         let mut owners: Owners = owners_response.json().await?;
 
@@ -25,54 +42,18 @@ pub async fn get_applications_with_owners(client: &GraphClient, ids: Vec<String>
     Ok(apps)
 }
 
-// DO NOT USE AT ALL.
-pub async fn get_all_applications_with_owners(client: &GraphClient) -> anyhow::Result<Vec<App>> {
-
-    let mut apps: Vec<App> = Vec::new();
-
-    let all_applications_response = client.applications().list_application().select(&["id", "appId", "displayName", "passwordCredentials"]).send().await?;
-
-    info!("Fetched all applications");
-
-    for application in all_applications_response.json::<serde_json::Value>().await?["value"].as_array().unwrap() {
-        let mut app: App = match serde_json::from_value(application.clone()) {
-            Ok(a) => a,
-            Err(e) => {
-                info!("Failed to parse application: {}. Skipping.", e);
-                continue;
-            }
-        };
-
-        let owners_response = client.application(&app.id).owners().list_owners().select(&["id", "displayName", "mail", "userPrincipalName"]).send().await?;
-
-        // If reading json fails, skip this application.
-        let owners: Owners = match owners_response.json::<Owners>().await {
-            Ok(o) => {
-                o
-            },
-            Err(_) => {
-                info!("Failed to parse owners for application '{:?}'. Skipping.", app.displayName);
-                continue;
-            }
-        };
-
-        app.insert_owners(owners.value);
-        apps.push(app);
-    }
-
-    info!("Found {} applications with owners", apps.len());
-
-    Ok(apps)
-
-}
-
 pub async fn get_all_applications_with_filter(client: &GraphClient) -> anyhow::Result<Vec<App>> {
     let mut apps: Vec<App> = Vec::new();
 
     // filter for application with passwordCredentials and owners.
     // ConsistencyLevel header must be set to "eventual" when using $count in filter.
-    let all_applications_response = client.applications().list_application()
-        .header(HeaderName::from_static("consistencylevel"), HeaderValue::from_static("eventual"))
+    let all_applications_response = client
+        .applications()
+        .list_application()
+        .header(
+            HeaderName::from_static("consistencylevel"),
+            HeaderValue::from_static("eventual"),
+        )
         .filter(&["owners/$count ne 0"])
         .select(&["id", "appId", "displayName", "passwordCredentials"])
         .count("true")
@@ -82,9 +63,7 @@ pub async fn get_all_applications_with_filter(client: &GraphClient) -> anyhow::R
 
     // all_application_response is a VecDeque of pages.
     for page in all_applications_response {
-
         for application_response in page.json() {
-            
             for application in application_response["value"].as_array().unwrap() {
                 let mut app: App = match serde_json::from_value(application.clone()) {
                     Ok(a) => a,
@@ -94,15 +73,22 @@ pub async fn get_all_applications_with_filter(client: &GraphClient) -> anyhow::R
                     }
                 };
 
-                let owners_response = client.application(&app.id).owners().list_owners().select(&["id", "displayName", "mail", "userPrincipalName"]).send().await?;
+                let owners_response = client
+                    .application(&app.id)
+                    .owners()
+                    .list_owners()
+                    .select(&["id", "displayName", "mail", "userPrincipalName"])
+                    .send()
+                    .await?;
 
                 // If reading json fails, skip this application.
                 let owners: Owners = match owners_response.json::<Owners>().await {
-                    Ok(o) => {
-                        o
-                    },
+                    Ok(o) => o,
                     Err(_) => {
-                        info!("Failed to parse owners for application '{:?}'. Skipping.", app.displayName);
+                        info!(
+                            "Failed to parse owners for application '{:?}'. Skipping.",
+                            app.displayName
+                        );
                         continue;
                     }
                 };
@@ -118,31 +104,66 @@ pub async fn get_all_applications_with_filter(client: &GraphClient) -> anyhow::R
     Ok(apps)
 }
 
-// Put the owners mail or userPrincipalName in a vec and return it
-pub fn check_expiring_credentials(apps: &Vec<App>) -> anyhow::Result<()> {
+// Return a list of owners and their corresponding expiring credentials.
+pub async fn check_expiring_credentials(
+    apps: &Vec<App>,
+) -> anyhow::Result<Vec<(String, Vec<String>, Vec<String>)>> {
+    // (App Name, Owner Emails, Expiring Credentials)
+    let mut alerts: Vec<(String, Vec<String>, Vec<String>)> = Vec::new();
+
     let now = chrono::Utc::now();
     let threshold = now + chrono::Duration::days(30);
 
-    // App Name, Vec<Owner Emails>
-    let mut app_owners: Vec<(String, Vec<String>)> = Vec::new();
-
     for app in apps {
+        let mut owner_emails: Vec<String> = Vec::new();
+        let mut expiring_credential_info: Vec<String> = Vec::new();
+
         if app.passwordCredentials.is_empty() {
-            info!("Application '{:?}' (App ID: {:?}) has no password credentials.", app.displayName, app.appId);
+            info!(
+                "Application '{:?}' (App ID: {:?}) has no password credentials.",
+                app.displayName, app.appId
+            );
             continue;
         }
         for credential in &app.passwordCredentials {
             if credential.endDateTime < threshold {
-                info!("Application '{:?}' (App ID: {:?}) has a credential expiring on {} (Key ID: {:?}, Hint: {:?})", app.displayName, app.appId, credential.endDateTime, credential.keyId, credential.hint);
+                info!(
+                    "Application '{:?}' (App ID: {:?}) has a credential expiring on {} (Key ID: {:?}, Hint: {:?})",
+                    app.displayName,
+                    app.appId,
+                    credential.endDateTime,
+                    credential.keyId,
+                    credential.hint
+                );
+                // Collect expiring credential info.
+                expiring_credential_info.push(format!(
+                    "Key ID: {:?}, Hint: {:?}, Expiry: {}",
+                    credential.keyId, credential.hint, credential.endDateTime
+                ));
+
+                // Collect owner emails.
                 if !app.owners.is_empty() {
                     info!("  Owners:");
                     for owner in &app.owners {
-                        if let Some(user_principal_name) = &owner.userPrincipalName {
-                            info!("    - {} ({})", owner.displayName.as_deref().unwrap_or("No Name"), user_principal_name);
-                        } else if let Some(mail) = &owner.mail {
-                            info!("    - {} ({})", owner.displayName.as_deref().unwrap_or("No Name"), mail);
+                        if let Some(mail) = &owner.mail {
+                            owner_emails.push(mail.clone());
+                            info!(
+                                "    - {} ({})",
+                                owner.displayName.as_deref().unwrap_or("No Name"),
+                                mail
+                            );
+                        } else if let Some(user_principal_name) = &owner.userPrincipalName {
+                            owner_emails.push(user_principal_name.clone());
+                            info!(
+                                "    - {} ({})",
+                                owner.displayName.as_deref().unwrap_or("No Name"),
+                                user_principal_name
+                            );
                         } else {
-                            info!("    - {} (No contact info)", owner.displayName.as_deref().unwrap_or("No Name"));
+                            info!(
+                                "    - {} (No contact info)",
+                                owner.displayName.as_deref().unwrap_or("No Name")
+                            );
                         }
                     }
                 } else {
@@ -150,7 +171,69 @@ pub fn check_expiring_credentials(apps: &Vec<App>) -> anyhow::Result<()> {
                 }
             }
         }
+
+        // If there are both expiring credentials and owner emails, add to alerts.
+        if !expiring_credential_info.is_empty() && !owner_emails.is_empty() {
+            alerts.push((
+                app.displayName
+                    .clone()
+                    .unwrap_or_else(|| "No Name".to_string()),
+                owner_emails,
+                expiring_credential_info,
+            ));
+        } else {
+            info!(
+                "No expiring credentials or no owners to notify for application '{:?}' (App ID: {:?})",
+                app.displayName, app.appId
+            );
+        }
     }
+
+    Ok(alerts)
+}
+
+pub async fn send_email_alert(
+    client: &GraphClient,
+    app_name: &str,
+    owner_emails: &Vec<String>,
+    expiring_credentials: &Vec<&str>,
+) -> anyhow::Result<()> {
+
+    
+    let alerting_email = std::env::var("ALERTING_EMAIL")?;
+    let reciever_email = std::env::var("RECIEVER_EMAIL")?;
+
+
+    info!(
+        "Sending email alert for application '{}' to owners: {:?} about expiring credentials: {:?}",
+        app_name, &reciever_email, expiring_credentials
+    );
+
+
+    let mail = client.user(&alerting_email)
+        .send_mail(&serde_json::json!({
+                "message": {
+                "subject": "Alert: Expiring Credentials for Application",
+                "body": {
+                    "contentType": "Text",
+                    "content": "The application '"
+                        .to_string() + app_name + "' has credentials expiring soon. Please review and take necessary action.\n\nExpiring Credentials:\n"
+                        + &expiring_credentials.join("\n")
+                },
+                "toRecipients":[
+              {
+                  "emailAddress":{
+                      "address": &reciever_email
+                  }
+              }
+          ]
+            },
+            "saveToSentItems": "true"
+        }
+        )).send().await?;
+
+    info!("Email sent with response: {:?}", mail);
+
     Ok(())
 }
 
@@ -166,14 +249,33 @@ async fn main() -> anyhow::Result<()> {
     // setup logging
     colog::init();
 
+    let app_ids = std::env::var("APPLICATION")?;
+    let app_ids: Vec<String> = app_ids.split(',').map(|s| s.trim().to_string()).collect();
+
+    // Initialize Graph client
     let client = client_secret_credential()?;
 
-    // let apps = get_all_applications_with_owners(&client).await?;
-    
-    let apps = get_all_applications_with_filter(&client).await?;
+    // let apps = get_all_applications_with_filter(&client).await?;
+    let apps = get_applications_with_owners(&client, app_ids).await?;
 
-    // Then we want to check each application for expiring credentials
-    check_expiring_credentials(&apps)?;
+    info!("Fetched {:?} applications with owners", apps);
 
+    let alerts = check_expiring_credentials(&apps).await?;
+
+    info!("Alerts!: {:?}", alerts);
+
+    // Send emails to owners of applications with expiring credentials.
+    for (app_name, owner_emails, expiring_credentials) in alerts {
+        if !owner_emails.is_empty() && !expiring_credentials.is_empty() {
+            let expiring_credentials_str: Vec<&str> =
+                expiring_credentials.iter().map(|s| s.as_str()).collect();
+            send_email_alert(&client, &app_name, &owner_emails, &expiring_credentials_str).await?;
+        } else {
+            info!(
+                "No owners or expiring credentials to notify for application '{}'",
+                app_name
+            );
+        }
+    }
     Ok(())
 }
